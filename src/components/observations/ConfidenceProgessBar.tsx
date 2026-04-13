615cfde9d47967963fc7b2e13a184a1991dc5798
unknown <pabilans@stud.ntnu.no>
2026-04-13 11:49:38 +0200

    added suggested changes in confidenceprogressbar.tsx abd index.css

--- src/components/observations/ConfidenceProgessBar.tsx
+++ src/components/observations/ConfidenceProgessBar.tsx
@@ -7,23 +7,21 @@ type ConfidenceProgressBarProps = {
 
 function ConfidenceProgressBar({ confidence }: ConfidenceProgressBarProps) {
   return (
-    <div className="flex flex-col md:flex-row md:items-center md:gap-4">
+    <div className="flex items-center gap-4">
       <Progress
         className={cn(
-          "md:basis-3/5",
-          confidence >= 1
-            ? "border border-green-800/50 *:data-[slot=progress-indicator]:bg-green-800"
-            : confidence >= 0.8
-              ? "border border-green-500/40 *:data-[slot=progress-indicator]:bg-green-500"
-              : confidence >= 0.6
-                ? "border border-yellow-500/40 *:data-[slot=progress-indicator]:bg-yellow-500"
-                : confidence >= 0.4
-                  ? "border border-orange-400/40 *:data-[slot=progress-indicator]:bg-orange-400"
-                  : "border border-red-700/50 *:data-[slot=progress-indicator]:bg-red-700",
+          "basis-3/5 border",
+          confidence >= 0.8
+            ? "border-success/50 *:data-[slot=progress-indicator]:bg-success"
+            : confidence >= 0.6
+              ? "border-caution/50 *:data-[slot=progress-indicator]:bg-caution"
+              : confidence >= 0.4
+                ? "border-warn/50 *:data-[slot=progress-indicator]:bg-warn"
+                : "border-destructive/50 *:data-[slot=progress-indicator]:bg-destructive",
         )}
         value={confidence * 100}
       />
-      <span className="py-2 md:basis-1/5">{`${(confidence * 100).toFixed(0)}%`}</span>
+      <span className="basis-1/5 py-2">{`${(confidence * 100).toFixed(0)}%`}</span>
     </div>
   );
 }
