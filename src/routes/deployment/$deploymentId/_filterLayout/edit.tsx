f905cbd6612a70a892db51024d5ecd67c762c603
Ivar Stende Simonsen <ivarssi@stud.ntnu.no>
2026-04-22 16:20:41 +0200

    fix: Fixed bug with name not being optional

--- src/routes/deployment/$deploymentId/_filterLayout/edit.tsx
+++ src/routes/deployment/$deploymentId/_filterLayout/edit.tsx
@@ -52,7 +52,7 @@ function EditPage({
 }) {
   const schema = z
     .object({
-      name: z.string().min(1, "Name cannot be empty"),
+      name: z.string().optional(),
       description: z.string().optional(),
       startDate: z.iso.date().optional(),
       endDate: z.iso.date().nullable().optional(),
